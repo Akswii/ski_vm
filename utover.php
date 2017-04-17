0a545bc2f38bf2ef7c4ea9ef7ab8619e71f3fe05
markus bugge <markus bugge>
2017-04-17 12:44:18 +0200

    mer commits

--- utover.php
+++ utover.php
@@ -1,8 +1,16 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+class Utover
+{
+                private $navn;
+                private $utover_id;
+                private $ovelse;
+                
+                public function get_navn () {return $this->navn;}
+                public function get_utover_id () {return $this->utover_id;}
+                public function get_ovelse () {return $this->ovelse;}
+                
+                public function set_navn ($navn) {$this->navn=$navn;}
+                public function set_utover_id ($utover_id) {$this->utover_id=$utover_id;}
+                public function set_ovelse ($ovelse) {$this->ovelse=$ovelse;}
+                }
